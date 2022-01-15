0fc11f35d916e948fdb9359666d09237b3079236
SimerusM <63352151+SimerusM@users.noreply.github.com>
2022-01-15 02:27:42 -0500

    Added footer + background gradient color changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,26 @@
-const Footer = () => {
-    return (
-        <h1>Footer</h1>
-    );
-}
+import React from "react";
+
+
+const Footer = () => (
+  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+      <div className="flex flex-[0.5] justify-center items-center">
+        {/* For any logo */}
+        {/* <img src={logo} alt="logo" className="w-32" /> */}
+      </div>
+    </div>
+
+    <div className="flex justify-center items-center flex-col mt-5">
+      <p className="text-white text-sm text-center">(Name of project)</p>
+    </div>
+
+    <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+
+    <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+      <p className="text-white text-left text-xs">@Simerus Mahesh and Scott Hao</p>
+      <p className="text-white text-right text-xs">CruzHacks</p>
+    </div>
+  </div>
+);
 
 export default Footer;
